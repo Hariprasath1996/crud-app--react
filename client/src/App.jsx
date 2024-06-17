cbca7b5bf76deecdcc178bbc901b08312b8ff53a
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-17 12:32:12 +0530

    created UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,21 +7,22 @@ function App() {
   return (
     <>
       <div className="container">
-        <h1>Crud App Create Using FrontEnd For React Js and BackEnd For Node Js</h1>
+        <h3>Crud App Create Using FrontEnd For React Js and BackEnd For Node Js</h3>
 <div className='input-container'>
-  <input type="Search" />
-  <button>Add Record</button>
+  <label htmlFor="Search" >Title : </label>
+  <input type="Search" id='Search' className='Search' />
+  <button className='search-btn'>Add Record</button>
 </div>
-<table>
-  <thead>
-    <tr>
+<table className='table-head'>
+  <thead >
+    <>
     <th>S.No</th>
     <th>Name</th>
     <th>Age</th>
     <th>City</th>
     <th>Edit</th>
     <th>Delete</th>
-    </tr>
+    </>
   </thead>
   <tbody>
     <tr>
@@ -29,24 +30,24 @@ function App() {
       <td>HariPrasath</td>
       <td>27</td>
       <td>Erode</td>
-      <td><button>Edit</button></td>
-      <td><button>Delete</button></td>
+      <td><button className='btn-edit green'>Edit</button></td>
+      <td><button className='btn-delete red'>Delete</button></td>
     </tr>
     <tr>
       <td>1</td>
       <td>HariPrasath</td>
       <td>27</td>
       <td>Erode</td>
-      <td><button>Edit</button></td>
-      <td><button>Delete</button></td>
+      <td><button className='btn-edit green'>Edit</button></td>
+      <td><button className='btn-delete red'>Delete</button></td>
     </tr>
     <tr>
       <td>1</td>
       <td>HariPrasath</td>
       <td>27</td>
       <td>Erode</td>
-      <td><button>Edit</button></td>
-      <td><button>Delete</button></td>
+      <td><button className='btn-edit green'>Edit</button></td>
+      <td><button className='btn-delete red'>Delete</button></td>
     </tr>
   </tbody>
 </table>
