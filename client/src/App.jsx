4839bd0271c29d2fc9aa7475c717df2886a2ce57
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-17 11:49:59 +0530

    create tables

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,8 +6,50 @@ function App() {
 
   return (
     <>
-      <div>
-
+      <div className="container">
+        <h1>Crud App Create Using FrontEnd For React Js and BackEnd For Node Js</h1>
+<div className='input-container'>
+  <input type="Search" />
+  <button>Add Record</button>
+</div>
+<table>
+  <thead>
+    <tr>
+    <th>S.No</th>
+    <th>Name</th>
+    <th>Age</th>
+    <th>City</th>
+    <th>Edit</th>
+    <th>Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>HariPrasath</td>
+      <td>27</td>
+      <td>Erode</td>
+      <td><button>Edit</button></td>
+      <td><button>Delete</button></td>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>HariPrasath</td>
+      <td>27</td>
+      <td>Erode</td>
+      <td><button>Edit</button></td>
+      <td><button>Delete</button></td>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>HariPrasath</td>
+      <td>27</td>
+      <td>Erode</td>
+      <td><button>Edit</button></td>
+      <td><button>Delete</button></td>
+    </tr>
+  </tbody>
+</table>
       </div>
     </>
   )
